1c98e79be2c464a6dd68d2f8e5696e96543e3a3f
george99g <georgi_99g@abv.bg>
2016-10-30 19:51:36 +0200

    Add QDiscordUser::update test

--- QDiscord/tests/auto/QDiscordUser/tst_qdiscorduser.cpp
+++ QDiscord/tests/auto/QDiscordUser/tst_qdiscorduser.cpp
@@ -9,6 +9,8 @@ public:
 private slots:
 	void testConstructor_data();
 	void testConstructor();
+	void testUpdate_data();
+	void testUpdate();
 private:
 	QJsonObject _testUser;
 	QJsonObject _nullUser;
@@ -90,6 +92,54 @@ void tst_QDiscordUser::testConstructor()
 	QCOMPARE(testBot.email(), output_email);
 	QCOMPARE(testBot.username(), output_username);
 	QVERIFY(testBot.verified() == output_verified);
+}
+
+void tst_QDiscordUser::testUpdate_data()
+{
+	QTest::addColumn<QDiscordUser>("original");
+	QTest::addColumn<QJsonObject>("update");
+	QTest::addColumn<QString>("output_id");
+	QTest::addColumn<QString>("output_avatar");
+	QTest::addColumn<bool>("output_bot");
+	QTest::addColumn<QString>("output_discriminator");
+	QTest::addColumn<QString>("output_email");
+	QTest::addColumn<QString>("output_username");
+	QTest::addColumn<bool>("output_verified");
+
+	QDiscordUser nullUser(_nullUser);
+
+	QTest::newRow("test1") << nullUser <<
+							  _testUser <<
+							  "111264179623531612" <<
+							  "577444852b" <<
+							  true <<
+							  "8701" <<
+							  "bot@example.org" <<
+							  "TestBot" <<
+							  true;
+}
+
+void tst_QDiscordUser::testUpdate()
+{
+	QFETCH(QDiscordUser, original);
+	QFETCH(QJsonObject, update);
+	QFETCH(QString, output_id);
+	QFETCH(QString, output_avatar);
+	QFETCH(bool, output_bot);
+	QFETCH(QString, output_discriminator);
+	QFETCH(QString, output_email);
+	QFETCH(QString, output_username);
+	QFETCH(bool, output_verified);
+
+	original.update(update);
+
+	QCOMPARE(original.id(), output_id);
+	QCOMPARE(original.avatar(), output_avatar);
+	QVERIFY(original.bot() == output_bot);
+	QCOMPARE(original.discriminator(), output_discriminator);
+	QCOMPARE(original.email(), output_email);
+	QCOMPARE(original.username(), output_username);
+	QVERIFY(original.verified() == output_verified);
 }
 
 QTEST_MAIN(tst_QDiscordUser)
