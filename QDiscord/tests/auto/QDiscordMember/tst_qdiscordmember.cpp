6b1bec6fa7600baca51f8f2a3f7c237d973e4150
george99g <georgi_99g@abv.bg>
2016-11-11 17:40:51 +0200

    Add QDiscordMember test.

---
+++ QDiscord/tests/auto/QDiscordMember/tst_qdiscordmember.cpp
@@ -0,0 +1,114 @@
+#include <QtTest>
+#include "qdiscord.d/qdiscordmember.hpp"
+#include "qdiscord.d/qdiscorduser.hpp"
+
+class tst_QDiscordMember : public QObject
+{
+	Q_OBJECT
+public:
+	tst_QDiscordMember();
+private slots:
+	void testConstructor_data();
+	void testConstructor();
+private:
+	QJsonObject _nullMember;
+	QJsonObject _guildlessMember;
+	QJsonObject _userlessMember;
+	QJsonObject _testMember;
+	QDiscordUser _testUser;
+};
+
+tst_QDiscordMember::tst_QDiscordMember():
+	_nullMember({
+			{"deaf", QJsonValue::Null},
+			{"mute", QJsonValue::Null},
+			{"nick", QJsonValue::Null},
+			{"joined_at", QJsonValue::Null}
+		}),
+	_userlessMember({
+			{"deaf", true},
+			{"mute", true},
+			{"nick", "testbot"},
+			{"joined_at", "2016-07-22T18:15:12.448000+00:00"}
+		}),
+	_testMember({
+			{"deaf", true},
+			{"mute", true},
+			{"nick", "testbot"},
+			{"joined_at", "2016-07-22T18:15:12.448000+00:00"},
+			{"user", QJsonObject({
+									 {"id", "111264179623531612"}
+									})}
+		}),
+	_testUser(
+		QJsonObject({
+			{"id", "111264179623531612"}
+		})
+		)
+{
+
+}
+
+void tst_QDiscordMember::testConstructor_data()
+{
+	QTest::addColumn<QJsonObject>("input_object");
+	QTest::addColumn<QDiscordUser>("output_user");
+	QTest::addColumn<bool>("user_is_null");
+	QTest::addColumn<bool>("output_deaf");
+	QTest::addColumn<bool>("output_mute");
+	QTest::addColumn<QDateTime>("output_joinedAt");
+	QTest::addColumn<QString>("output_nickname");
+
+	QTest::newRow("nullMember") << _nullMember <<
+								   _testUser <<
+								   true <<
+								   false <<
+								   false <<
+								   QDateTime() <<
+								   "";
+
+	QTest::newRow("testMember") << _testMember <<
+								   _testUser <<
+								   false <<
+								   true <<
+								   true <<
+								   QDateTime::fromString(
+									   "2016-07-22T18:15:12.448000+00:00",
+									   Qt::ISODate) <<
+								   "testbot";
+	QTest::newRow("userlessMember") << _userlessMember <<
+								   _testUser <<
+								   true <<
+								   true <<
+								   true <<
+								   QDateTime::fromString(
+									   "2016-07-22T18:15:12.448000+00:00",
+									   Qt::ISODate) <<
+								   "testbot";
+}
+
+void tst_QDiscordMember::testConstructor()
+{
+	QFETCH(QJsonObject, input_object);
+	QFETCH(QDiscordUser, output_user);
+	QFETCH(bool, user_is_null);
+	QFETCH(bool, output_deaf);
+	QFETCH(bool, output_mute);
+	QFETCH(QDateTime, output_joinedAt);
+	QFETCH(QString, output_nickname);
+
+	QDiscordMember member(input_object, nullptr);
+
+	if(!user_is_null)
+		QVERIFY(*member.user() == output_user);
+	else
+		QVERIFY(member.user() == nullptr);
+	QVERIFY(member.deaf() == output_deaf);
+	QVERIFY(member.mute() == output_mute);
+	QVERIFY(member.joinedAt() == output_joinedAt);
+	QCOMPARE(member.nickname(), output_nickname);
+}
+
+QTEST_MAIN(tst_QDiscordMember)
+
+#include "tst_qdiscordmember.moc"
